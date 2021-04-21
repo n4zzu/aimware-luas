ac56e054308683f85647938249e4cfe851d7aaa5
naz <42706009+n4zzu@users.noreply.github.com>
2021-04-21 14:48:47 +0100

    Update SkinSeedSlider.lua

--- SkinSeedSlider.lua
+++ SkinSeedSlider.lua
@@ -1,3 +1,4 @@
+local visualsRef = gui.Reference("Visuals")
 local seedRef = gui.Reference("Visuals", "Skins", "Configuration", "Seed")
 local stattrackRef = gui.Reference("Visuals", "Skins", "Configuration", "StatTrak")
 local sliderRef = gui.Reference("Visuals", "Skins", "Configuration")
@@ -6,12 +7,22 @@ local sliderRef2 = gui.Reference("Visuals", "Skins", "Configuration", "Skin Seed
 local slider = gui.Slider(sliderRef, "swaggin", "Skin Seed", 1, 0, 999)
 slider:SetDescription("Set the seed for the skin.")
 
+local skinTab = gui.Tab(visualsRef, "skinTab", "Skin Seeds")
+local mainGroup = gui.Groupbox(skinTab, "Skin Seeds", 16, 16, 605, 100)
+
+gui.Text(mainGroup, "Karambit Case Hardened - seed 231 - Full Gold")
+gui.Text(mainGroup, "Karambit Case Hardened - seed 442 - Playside 100% Cloudy Blue Gem")
+gui.Text(mainGroup, "Karambit Doppler1 - seed 583 - Best Ruby Seed")
+gui.Text(mainGroup, "M9 Bayonet Case Hardened - seed 72 - Full Gold")
+gui.Text(mainGroup, "M9 Bayonet Case Hardened - seed 58 - Playside Blue Gem (75% - 80%)")
+gui.Text(mainGroup, "M9 Bayonet Case Hardened - seed 601 - OCEANO")
+gui.Text(mainGroup, "Butterfly Knife Case Hardened - seed 510 - Blue Gem")
+
 function removeSeed()
     gui.Reference("Visuals", "Skins", "Configuration", "Seed"):SetInvisible(true)
     stattrackRef:SetPosY(280)
     slider:SetPosY(210)
     seedRef:SetValue(tostring(slider:GetValue()))
-    print(slider:GetValue())
 end
 
 callbacks.Register("Draw", removeSeed)
